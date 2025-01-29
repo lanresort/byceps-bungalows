88a7a23c04fa7538e06520da1f49749319e0ceac
Jochen Kupperschmidt <homework@nwsnet.de>
2025-01-29 01:54:16 +0100

    Stabilize bungalow ticket and slot order by using ticket ID, not creation time
    Tickets for an order are created at the same time, so that makes the order unstable.

--- byceps/services/bungalow/bungalow_occupancy_service.py
+++ byceps/services/bungalow/bungalow_occupancy_service.py
@@ -579,7 +579,7 @@ def get_occupant_slots_for_occupancies(
         )
         .join(DbTicket)
         .filter(DbBungalowOccupancy.id.in_(occupancy_ids))
-        .order_by(DbTicket.created_at)
+        .order_by(DbTicket.id)
     ).all()
 
     user_ids = {user_id for _, _, user_id in rows if user_id is not None}
