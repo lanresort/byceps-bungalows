65531b924642f700051f9b0803d9652a0e47b75d
Jochen Kupperschmidt <homework@nwsnet.de>
2025-03-08 04:38:15 +0100

    Adjust to permission modules having been moved into service packages

--- byceps/services/bungalow/permissions.py
+++ byceps/services/bungalow/permissions.py
@@ -1,6 +1,6 @@
 """
-byceps.permissions.bungalow
-~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.bungalow.permissions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
