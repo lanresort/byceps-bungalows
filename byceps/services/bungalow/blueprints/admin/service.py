2bd17b3cb8898f207f75259c2244067093bba209
Jochen Kupperschmidt <homework@nwsnet.de>
2025-04-02 01:13:18 +0200

    Move admin blueprint into corresponding service package

--- byceps/services/bungalow/blueprints/admin/service.py
+++ byceps/services/bungalow/blueprints/admin/service.py
@@ -1,6 +1,6 @@
 """
-byceps.blueprints.admin.bungalow.service
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.bungalow.blueprints.admin.service
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
